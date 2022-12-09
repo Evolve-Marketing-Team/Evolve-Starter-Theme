2f4fcca0dbde76302b1368056ef130324f314e75
alexf <alexf@evolvemarketingteam.com>
2022-12-09 10:30:38 -0500

    💄 Add base block styles for lists and buttons

--- src/js/editor/editor.js
+++ src/js/editor/editor.js
@@ -10,6 +10,44 @@ wp.domReady( () => {
 		{
 			name: 'list-style-none',
 			label: 'List Style None',
+		},
+
+		{
+			name: 'two-column',
+			label: 'Two Column List',
 		}
+	]);
+
+	// Disable core button defaults in favor of our button color scheme
+	wp.blocks.unregisterBlockStyle( 'core/button', 'default' );
+	wp.blocks.unregisterBlockStyle( 'core/button', 'fill' );
+	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
+	wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
+
+	// Setup your theme button color schemes. 
+	wp.blocks.registerBlockStyle( 'core/button', [
+		{
+			name: 'black-btn',
+			label: 'Black Button',
+			isDefault: true,
+		},
+
+		{
+			name: 'white-btn',
+			label: 'White Button',
+		},
+	]);
+	
+	wp.blocks.registerBlockStyle( 'core/group', [
+		{
+			name: 'default',
+			label: 'Default',
+			isDefault: true,
+		},
+
+		{
+			name: 'full-width-content',
+			label: 'Full Width',
+		},
 	]);
 });
