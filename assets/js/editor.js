616af03ab6c99cf8c02e133c7c6ccd74dfa4595d
alexf <alexf@evolvemarketingteam.com>
2023-02-01 15:51:49 -0500

    📦 Compiled updates

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,6 +1,6 @@
 "use strict";
 
-wp.domReady(function () {
+wp.domReady(() => {
   wp.blocks.registerBlockStyle('core/list', [{
     name: 'default',
     label: 'Default',
@@ -8,5 +8,33 @@ wp.domReady(function () {
   }, {
     name: 'list-style-none',
     label: 'List Style None'
-  }]);
+  }, {
+    name: 'two-column',
+    label: 'Two Column List'
+  }]); // Disable core button defaults in favor of our button color scheme
+
+  wp.blocks.unregisterBlockStyle('core/button', 'default');
+  wp.blocks.unregisterBlockStyle('core/button', 'fill');
+  wp.blocks.unregisterBlockStyle('core/button', 'outline');
+  wp.blocks.unregisterBlockStyle('core/button', 'squared'); // Setup your theme button color schemes. 
+
+  wp.blocks.registerBlockStyle('core/button', [{
+    name: 'black-btn',
+    label: 'Black Button',
+    isDefault: true
+  }, {
+    name: 'white-btn',
+    label: 'White Button'
+  }]); // Need more control for Group block - uncomment code below.
+  // wp.blocks.registerBlockStyle( 'core/group', [
+  // 	{
+  // 		name: 'default',
+  // 		label: 'Default',
+  // 		isDefault: true,
+  // 	},
+  // 	{
+  // 		name: 'full-width-content',
+  // 		label: 'Full Width',
+  // 	},
+  // ]);
 });
